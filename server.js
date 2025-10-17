5dd7193bdf6bd4c2fab92e1d10579d4cf96d76ad
IT21142628 <it21142628@my.sliit.lk>
2025-10-17 05:46:10 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 const http = require('http');
+const url = require('url');
 
+const server= http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview'){
+        res.end('overview');
+    }
+    else if(pathName === '/product'){
+        res.end('this is PRODUCT');
+    }
+    else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello world'
+        });
+        res.end('<h1>Page not found</h1>')
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('listen to 8000');
+})
